2d605c0fbabe5f5c90bb89766bca66a6446c432f
Inti C. Velasquez Leiva <intiv@unitec.edu>
2016-05-06 16:57:24 -0600

    Ya hace los turnos bien y dice el ganador, todavia no revisa diagonal

--- Lab3.cpp
+++ Lab3.cpp
@@ -19,7 +19,7 @@ int main(int argc,char* argv[]){
 		}
 	}
 	int turno=1;
-	bool ganado=false;
+	bool ganado=false,MatrizLlena=false;
 	//imprimir(tablero);
 	while(!ganado){
 		for(int i=0;i<lineas;i++){
@@ -29,7 +29,8 @@ int main(int argc,char* argv[]){
 			cout<<endl;
 		}
 		int x=1;
-		if(turno){
+		if(turno==1){
+			cout<<"Turno del jugador 1!"<<endl;
 			while(x==1){
 				column=input();
 				int validar=6,currLine;
@@ -84,25 +85,24 @@ int main(int argc,char* argv[]){
 							ganado=true;
 						}
 					}
-				}
-				x=1;	
+				}	
 			}
-		}else{
-						
-			while(x==1){
+		}else{	
+			cout<<"Turno del jugador 2!"<<endl;
+			int turnValido=1;		
+			while(turnValido==1){
 				column=input();
 				int validar=6,currLine;
 				for(int i=lineas-1;i>=0;i--){
 					if(tablero[i][column]==0){
-						tablero[i][column]=1;
+						tablero[i][column]=2;
 						currLine=i;
 						i=0;
-						x=2;
+						turnValido=2;
 					}else{
 						validar--;
 					}
 				}
-				cout<<validar<<endl;
 				if(validar==0){
 					cout<<"Esa columna esta llena, ingrese una columna diferente"<<endl;
 				}else{
@@ -143,12 +143,35 @@ int main(int argc,char* argv[]){
 							ganado=true;
 						}
 					}
-			}	
-		}
-
-
+					if(!ganado){
+						int jugadas=0;
+						for(int i=0;i<lineas;i++){
+							for(int j=0;j<columnas;j++){
+								if(tablero[i][j]!=0){
+									jugadas++;
+								}
+							}
+	
+						}
+						if(jugadas==42){
+							ganado=true;
+						}
+					}
+				}
+			}
+				
 		}
 		if(turno==1){ turno=2; }else{ turno=1; }
+	}
+	if(MatrizLlena){
+		cout<<"La matriz esta llena y ninguno de los 2 jugadores ha ganado!"<<endl;
+	}else{	
+	
+		if(turno==2){
+			cout<<"El jugador 1 es el ganador!"<<endl;
+		}else{
+			cout<<"El jugador 2 es el ganador!"<<endl;
+		}
 	}
 	return 0;
 
